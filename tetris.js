3d57a73e841d941b288687f698d2be14be938729
Artur Oganesyan <artur.og@yahoo.com>
2017-04-17 21:00:29 -0500

    Add score to the game!

--- tetris.js
+++ tetris.js
@@ -4,6 +4,7 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 function arenaSweep() {
+	let rowCount = 1;
 	 outer: for (let y = arena.length - 1; y > 0; --y) {
 		for (let x = 0; x < arena[y].length; ++x) {
 			if (arena[y][x] === 0) {
@@ -13,6 +14,9 @@ function arenaSweep() {
 		const row = arena.splice(y, 1)[0].fill(0);
 		arena.unshift(row);
 		++y;
+
+		player.score += rowCount * 10;
+		rowCount *= 2;
 	}
 }
 
@@ -121,6 +125,7 @@ function playerDrop() {
 		merge(arena, player);
 		playerReset(); 
 		arenaSweep();
+		updateScore();
 	}
 	dropCounter = 0;	
 }
@@ -140,6 +145,8 @@ function playerReset() {
 				   (player.matrix[0].length / 2 | 0);
 	if(collide(arena, player)) {
 		arena.forEach(row => row.fill(0));
+		player.score = 0;
+		updateScore();
 	}
 }
 
@@ -195,6 +202,10 @@ function update(time = 0) {
 	requestAnimationFrame(update);
 }
 
+function updateScore() {
+	document.getElementById('score').innerText = player.score;
+}
+
 const colors = [
 	null,
 	'#69D2E7',
@@ -209,8 +220,9 @@ const colors = [
 const arena = createMatrix(12, 20);
 
 const player = {
-	pos: {x: 5, y: 5},
-	matrix: createPiece('T'),
+	pos: {x: 0, y: 0},
+	matrix: null,
+	score: 0,
 }
 
 document.addEventListener('keydown', event => {
@@ -231,4 +243,6 @@ document.addEventListener('keydown', event => {
 	}
 });
 
+playerReset();
+updateScore();
 update();
