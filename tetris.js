639383fabd3ae672ca534c33daaa0e442d7b2453
Artur Oganesyan <artur.og@yahoo.com>
2017-04-17 20:36:20 -0500

    Add colors to our boxes!

--- tetris.js
+++ tetris.js
@@ -38,38 +38,38 @@ function createPiece(type) {
 		];
 	} else if (type === 'O') {
 		return [
-			[1,1],
-			[1,1],
+			[2,2],
+			[2,2],
 		];
 	} else if (type === 'L') {
 		return [
-			[0, 1, 0],
-			[0, 1, 0],
-			[0, 1, 1],
+			[0, 3, 0],
+			[0, 3, 0],
+			[0, 3, 3],
 		];
 	} else if (type === 'J') {
 		return [
-			[0, 1, 0],
-			[0, 1, 0],
-			[1, 1, 0],
+			[0, 4, 0],
+			[0, 4, 0],
+			[4, 4, 0],
 		];
 	} else if (type === 'I') {
 		return [
-			[0, 1, 0, 0],
-			[0, 1, 0, 0],
-			[0, 1, 0, 0],
-			[0, 1, 0, 0],
+			[0, 5, 0, 0],
+			[0, 5, 0, 0],
+			[0, 5, 0, 0],
+			[0, 5, 0, 0],
 		];
 	} else if (type === 'S') {
 		return [
-			[0, 1, 1],
-			[1, 1, 0],
+			[0, 6, 6],
+			[6, 6, 0],
 			[0, 0, 0],
 		];
 	} else if (type === 'Z') {
 		return [
-			[1, 1, 0],
-			[0, 1, 1],
+			[7, 7, 0],
+			[0, 7, 7],
 			[0, 0, 0],
 		];
 	} 
@@ -87,7 +87,7 @@ function drawMatrix(matrix, offset) {
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
 			if (value !== 0) {
-				context.fillStyle = 'red';
+				context.fillStyle = colors[value];
 				context.fillRect(x + offset.x,
 								 y + offset.y, 1, 1)
 			}
@@ -185,8 +185,18 @@ function update(time = 0) {
 	requestAnimationFrame(update);
 }
 
+const colors = [
+	null,
+	'#69D2E7',
+	'#E94C6F',
+	'#DB3340',
+	'#D0C91F',
+	'#00C8F8',
+	'#DE4D4E',
+	'#260126',
+];
+
 const arena = createMatrix(12, 20);
-console.log(arena); console.table(arena);
 
 const player = {
 	pos: {x: 5, y: 5},
